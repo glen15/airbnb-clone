1fcc2e935975846d5d4afe2353ddd8ae1d183ca6
glen15 <75651376+glen15@users.noreply.github.com>
2021-04-09 12:33:45 +0900

    #11.8 class based view

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from rooms import views as room_views
 app_name = "core"  # config에 있는 urls.py에 있는 urlpatterns의 spacename 과 일치해야한다
 
 # urlpatterns 변수명은 필수, 옵션이 아님
-urlpatterns = [path("", room_views.all_rooms, name="home")]
+urlpatterns = [path("", room_views.HomeView.as_view(), name="home")]
 # rooms 어플폴더에서 views.py 파일을 room_views라는 이름으로 import 해놓은 경로로 가져와서 그안 에 있는 all_rooms 함수를 실행시킨것
--- rooms/views.py
+++ rooms/views.py
@@ -1,20 +1,13 @@
-from math import ceil
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = (
-        models.Room.objects.all()
-    )  # 쿼리셋만 만들뿐 즉시 불러오는건 아니다 = 데이터베이스에 all이 다들어가는 사태는 안일어나
-    paginator = Paginator(
-        room_list, 10, orphans=5
-    )  # orphans 남는애들인데 이렇게 5로 지정하면 5개까지는 마지막페이지에 넣어서 보여줌
+class HomeView(ListView):  # core폴더의 url에도 넣어뒀음
 
-    try:
-        rooms = paginator.page(int(page))  # 11.3 commit에 수동으로한걸 자동으로 해줌
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")  # 홈으로 리다이렉트 시켜서 유저가 url을 막써놓은걸 다시 정리 시켜주는 것
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10  # ListView 관련사항 ccv.co.uk확인하면 잘 정리되어있음
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
