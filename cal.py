c799e92ad3c4cbe218cc345b90c3daa2d9ff7f62
glen15 <75651376+glen15@users.noreply.github.com>
2021-05-04 20:26:45 +0900

    #24.1 calendar part two
    달력만들기 다시 해볼 것

--- cal.py
+++ cal.py
@@ -3,11 +3,12 @@
 import calendar
 
 
-class Calendar:
+class Calendar(calendar.Calendar):
     def __init__(self, year, month):
+        super().__init__(firstweekday=6)
         self.year = year
         self.month = month
-        self.day_names = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+        self.day_names = ("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
         self.months = (
             "January",
             "February",
@@ -22,6 +23,18 @@ class Calendar:
             "November",
             "December",
         )
+
+    def get_days(self):
+        weeks = self.monthdays2calendar(self.year, self.month)
+        days = []
+        for week in weeks:
+            for (
+                day,
+                _,
+            ) in week:  # for 다음 변수 두개넣어서 튜플로 된 애를 앞 뒤 하나 씩 따로 변수받아냄( _ 는 무시하라는 뜻)
+                days.append(day)
+
+        return days
 
     def get_month(self):
         return self.months[self.month - 1]
